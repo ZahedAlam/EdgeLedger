18d2c929747a172dc318f000b0007deadb2126d6
Zahed Alam <zahed@innof.com>
2021-03-08 13:36:43 +0100

    jQuery with smooth scrolling added

--- js/main.js
+++ js/main.js
@@ -1,47 +1,16 @@
-import 'ol/ol.css';
-import BingMaps from 'ol/source/BingMaps';
-import Map from 'ol/Map';
-import TileLayer from 'ol/layer/Tile';
-import View from 'ol/View';
+//Smooth Scrolling
 
-var styles = [
-  'RoadOnDemand',
-  'Aerial',
-  'AerialWithLabelsOnDemand',
-  'CanvasDark',
-  'OrdnanceSurvey' ];
-var layers = [];
-var i, ii;
-for (i = 0, ii = styles.length; i < ii; ++i) {
-  layers.push(
-    new TileLayer({
-      visible: false,
-      preload: Infinity,
-      source: new BingMaps({
-        key: 'Your Bing Maps Key from http://www.bingmapsportal.com/ here',
-        imagerySet: styles[i],
-        // use maxZoom 19 to see stretched tiles instead of the BingMaps
-        // "no photos at this zoom level" tiles
-        // maxZoom: 19
-      }),
-    })
-  );
-}
-var map = new Map({
-  layers: layers,
-  target: 'map',
-  view: new View({
-    center: [-6655.5402445057125, 6709968.258934638],
-    zoom: 13,
-  }),
-});
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
 
-var select = document.getElementById('layer-select');
-function onChange() {
-  var style = select.value;
-  for (var i = 0, ii = layers.length; i < ii; ++i) {
-    layers[i].setVisible(styles[i] === style);
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 50
+      },
+      800
+    );
   }
-}
-select.addEventListener('change', onChange);
-onChange();
+});
